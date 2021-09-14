fdec9cc38a165d539f9c6b5d1cf603b863956ed1
LeaSuLinHabel <lea.habel@yahoo.de>
2021-09-14 17:28:45 +0200

    NavBar mit Dropdown aufgenommen, noch nicht angepasst

--- src/App.js
+++ src/App.js
@@ -12,9 +12,19 @@ import Home from './pages';
 import Service from './pages/servicepage';
 import Personpage from './pages/personpage';
 
+import Navbar from './components/NavBarTutorial/Navbar';
+import Home2 from './components/NavBarTutorial/pages/Home2';
+import Services from './components/NavBarTutorial/pages/Services';
+import Products from './components/NavBarTutorial/pages/Products';
+import ContactUs from './components/NavBarTutorial/pages/ContactUs';
+import SignUp from './components/NavBarTutorial/pages/SignUp';
+import Marketing from './components/NavBarTutorial/pages/Marketing';
+import Consulting from './components/NavBarTutorial/pages/Consulting';
 
-import { Switch, Route } from 'react-router';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+{/*import { Switch, Route } from 'react-router';*/ }
 {/* https://www.youtube.com/watch?v=gOQ31Kc8H5E&t=437s&ab_channel=BrianDesign 43:15*/ }
+
 
 
 export default function App() {
@@ -42,14 +52,27 @@ export default function App() {
 
   return (
     <>
-      <Navigationbar toggle={toggle} /> {/* passing in  toggle to the Navbar component*/}
-      <Dropdown isOpen={isOpen} toggle={toggle} />
+      {/* <Navigationbar toggle={toggle} /> passing in  toggle to the Navbar component*/}
+      {/* <Dropdown isOpen={isOpen} toggle={toggle} /> */}
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home2} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/contact-us' component={ContactUs} />
+          <Route path='/sign-up' component={SignUp} />
+          <Route path='/marketing' component={Marketing} />
+          <Route path='/consulting' component={Consulting} />
+        </Switch>
+      </Router>
       <Switch> {/* switch makes sure that only one Route will be shown*/}
         {/* This will be extra pages for further information. Addition to Onepager.  */}
         <Route path="/" exact component={Home} /> {/*path is the path in the URL name. example: website.com/pathname*/}
         <Route path="/service" component={Service} />
         <Route path="/person" component={Personpage} />
       </Switch>
+
 
       <Contact />
       <Footer />
---
+++ src/components/NavBarTutorial/pages/Home2.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Home2() {
+  return (
+    <>
+      <h1 className='home'>EPIC</h1>
+    </>
+  );
+}
